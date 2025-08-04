ca98736f1166dc8f9fae48b64466c4b79700d31f
SpainBingo Deploy <deploy@spainbingo.es>
2025-08-04 15:50:27 +0200

    Cambiar a modo fork para evitar problemas de cluster

--- public/ecosystem.config.js
+++ public/ecosystem.config.js
@@ -4,6 +4,7 @@ module.exports = {
     script: 'server.js',
     cwd: '/home/ec2-user',
     instances: 1,
+    exec_mode: 'fork',
     autorestart: true,
     watch: false,
     max_memory_restart: '1G',
