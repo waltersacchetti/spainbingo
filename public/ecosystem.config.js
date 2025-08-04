492f8a6ec51a7ddd479714014eec31e3e53448cf
SpainBingo Deploy <deploy@spainbingo.es>
2025-08-04 16:59:57 +0200

    Corregir endpoint de base de datos en ecosystem.config.js

--- public/ecosystem.config.js
+++ public/ecosystem.config.js
@@ -11,7 +11,7 @@ module.exports = {
     env: {
       NODE_ENV: 'production',
       PORT: 3000,
-      DB_HOST: 'spainbingo-db.cqjqjqjqjqjq.eu-west-1.rds.amazonaws.com',
+      DB_HOST: 'spainbingo-db.clzgxn85wdjh.eu-west-1.rds.amazonaws.com',
       DB_PORT: 5432,
       DB_NAME: 'spainbingo',
       DB_USERNAME: 'spainbingo_admin',
